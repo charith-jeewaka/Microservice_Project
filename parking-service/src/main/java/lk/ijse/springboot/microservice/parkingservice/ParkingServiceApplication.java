3cbcdd2edb21168f029d56fdcbd8468e0cbae9e8
W.A.Charith Jeewaka <wickramasinghajeewaka@gmail.com>
2026-08-16 17:03:36 +0530

    empty parking service module

---
+++ parking-service/src/main/java/lk/ijse/springboot/microservice/parkingservice/ParkingServiceApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.springboot.microservice.parkingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingServiceApplication.class, args);
+    }
+
+}

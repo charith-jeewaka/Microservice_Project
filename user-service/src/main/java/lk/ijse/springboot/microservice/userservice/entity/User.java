2d6b3f90950245d93252b37c692d5610a0202ed5
W.A.Charith Jeewaka <wickramasinghajeewaka@gmail.com>
2026-08-16 14:08:21 +0530

    user entity created

---
+++ user-service/src/main/java/lk/ijse/springboot/microservice/userservice/entity/User.java
@@ -0,0 +1,34 @@
+package lk.ijse.springboot.microservice.userservice.entity;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+    
+    @Column(nullable = false)
+    private String password;
+
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+}
